3e8e31b9afd9cbd5e25e65f0b0c68c4e15571e4a
Sem Postma <lestergallagher18@gmail.copm>
2020-02-27 15:18:13 +0100

    Initial

--- es/escape-regex.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (regex) {
-    return regex.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-});
--- es/str-repeat.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (str, n) {
-    if (n === 0)
-        return '';
-    n = n || 1;
-    return Array(n + 1).join(str);
-});
